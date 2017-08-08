3501f42102e5aac944ca4cf5e8a11efb2dc6a9b2
Bepartofyou <309554135@qq.com>
2017-08-08 20:22:30 +0800

    add cat command examples

---
+++ shells/cat/demo-1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cat << END
+
+#ifndef CAT_DEMO
+#define CAT_DEMO 1
+#endif
+
+END
---
+++ shells/cat/demo-2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cat << END  >> cat.h
+
+#ifndef CAT_DEMO
+#define CAT_DEMO 1
+#endif
+
+END
